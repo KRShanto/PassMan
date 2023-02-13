770c425a73372b4ec45fbabd810ee08f824fbeb7
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2023-02-13 23:25:36 +0600

    Create component `Passwords` to display the passwords

---
+++ components/dashboard/Passwords.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { IPassword } from "../../models/password";
+import usePopupStore from "../../stores/popup";
+
+export default function Passwords({ passwords }: { passwords: IPassword[] }) {
+  const openPopup = usePopupStore((state) => state.openPopup);
+
+  console.log("Passwords: ", passwords);
+
+  return (
+    <div>
+      <div className="options">
+        <button
+          className="btn blue"
+          onClick={() => {
+            openPopup("CreatePassword");
+          }}
+        >
+          Create Password
+        </button>
+      </div>
+
+      <ul>
+        {passwords.map((password: IPassword, index: number) => (
+          <li key={index}>
+            <p>Title: {password.title}</p>
+            <p>Website: {password.website}</p>
+            <p>Password: {password.password}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
