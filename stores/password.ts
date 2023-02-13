e116c65402b8a19c8eeac73f588b4857da8820d1
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2023-02-13 23:21:59 +0600

    Create a password store

---
+++ stores/password.ts
@@ -0,0 +1,36 @@
+import { create } from "zustand";
+import { IPassword } from "../models/password";
+
+interface PasswordsState {
+  passwords: IPassword[];
+  addPassword: (password: IPassword) => void;
+  addMultiplePasswords: (passwords: IPassword[]) => void;
+  removePassword: (passwordId: string) => void;
+  updatePassword: (password: IPassword) => void;
+}
+
+const usePasswordsStore = create<PasswordsState>((set) => ({
+  passwords: [],
+
+  addPassword: (password) =>
+    set((state) => ({ passwords: [...state.passwords, password] })),
+
+  addMultiplePasswords: (passwords) =>
+    set((state) => ({ passwords: [...state.passwords, ...passwords] })),
+
+  removePassword: (passwordId) =>
+    set((state) => ({
+      passwords: state.passwords.filter(
+        (password) => password._id !== passwordId
+      ),
+    })),
+
+  updatePassword: (password) =>
+    set((state) => ({
+      passwords: state.passwords.map((oldPassword) =>
+        oldPassword._id === password._id ? password : oldPassword
+      ),
+    })),
+}));
+
+export default usePasswordsStore;
