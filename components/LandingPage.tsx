727eeb2ca7f578781a7dc9e920f5a57f32e85ab5
KR Shanto <krshanto2005@gmail.com>
2023-02-19 11:06:33 +0600

    Style the landing page

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,28 +1,81 @@
 import React from "react";
 import { signIn } from "next-auth/react";
+import Image from "next/image";
+import KeyImg from "../public/key.svg";
+import GlobeImg from "../public/globe.svg";
+import PasswordImg from "../public/password.svg";
+import ShareImg from "../public/share.svg";
+import SaveImg from "../public/save.svg";
+
+const ICON_SIZE = 30;
 
 export default function LandingPage() {
   return (
-    <div className="LandingPage">
+    <div id="LandingPage">
       <div className="hero">
-        <h1>PassMan</h1>
-        <p>Keep all your passwords secure and organized in one place.</p>
+        <div className="logo">
+          <Image src={KeyImg} alt="Key" width={70} height={70} />
+          <h1 className="text">PassMan</h1>
+        </div>
+        <p className="short-details">
+          Keep all your passwords secure and organized in one place.
+        </p>
       </div>
 
       <div className="features">
-        <h2>Features</h2>
+        <h2 className="header">Features</h2>
         <ul>
-          <li>Securely store your passwords</li>
-          <li>Generate strong passwords</li>
-          <li>Share passwords with others</li>
-          <li>Access your passwords from anywhere</li>
+          <li>
+            <Image
+              src={SaveImg}
+              alt="Save"
+              width={ICON_SIZE}
+              height={ICON_SIZE}
+            />
+            <p className="text">Securely store your passwords</p>
+          </li>
+
+          <li>
+            <Image
+              src={PasswordImg}
+              alt="Password"
+              width={ICON_SIZE}
+              height={ICON_SIZE}
+            />
+            <p className="text">Generate strong passwords</p>
+          </li>
+
+          <li>
+            <Image
+              src={GlobeImg}
+              alt="Globe"
+              width={ICON_SIZE}
+              height={ICON_SIZE}
+            />
+            <p className="text">Access your passwords from anywhere</p>
+          </li>
+
+          <li>
+            <Image
+              src={ShareImg}
+              alt="Share"
+              width={ICON_SIZE}
+              height={ICON_SIZE}
+            />
+            <p className="text">
+              Share passwords with others
+              <span className="future"> (coming soon)</span>
+            </p>
+          </li>
         </ul>
       </div>
 
-      <div className="join">
-        <button onClick={() => signIn()}>Sign up for free</button>
-        <button>Install the browser extension</button>
-        <button>Install the mobile app</button>
+      <div className="options">
+        <button className="btn blue" onClick={() => signIn()}>
+          Sign up for free
+        </button>
+        <button className="btn disable">Install the browser extension</button>
+        <button className="btn disable">Install the mobile app</button>
       </div>
     </div>
   );
